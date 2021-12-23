7b638a7266b1e615f30c3f8e12486374afb66af2
Chris Hatcher <hatcherchris20@gmail.com>
2021-12-23 22:07:28 +0000

    adding hello file

--- hello.js
+++ hello.js
@@ -4,7 +4,13 @@ const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
 
-
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
